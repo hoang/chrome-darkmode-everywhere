a2c85e7486906c9700c82b9611a608338b63e1e9
Trần Vũ Hoàng <hoangtv@tcbs.com.vn>
2020-09-30 10:51:05 +0700

    revert previous commit due to not work with iframe

--- popup.js
+++ popup.js
@@ -5,9 +5,7 @@ applyButton.onclick = function(element) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {
-                code: 'var newStyleElm = document.createElement("style"); newStyleElm.innerHTML = ".inverted-root {filter: invert(0.8);} .inverted-root img {filter: invert(1);}"; document.body.append(newStyleElm); document.documentElement.setAttribute("class", "inverted-root");'
-            }
+            {code: 'document.documentElement.style.filter = "invert(80%)";'}
         )
     })
 }
@@ -16,7 +14,7 @@ removeButton.onclick = function(element) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'document.documentElement.setAttribute("class", "");'}
+            {code: 'document.documentElement.style.filter = "initial";'}
         )
     })
 }
