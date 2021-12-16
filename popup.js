ce0f954b2551e406c59d0008df872ee97a4d7c6a
hoang <hoangcoltech@gmail.com>
2021-12-17 04:14:04 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,20 +1,28 @@
 let applyButton = document.getElementById('apply-darkmode');
 let removeButton = document.getElementById('remove-darkmode');
 
-applyButton.onclick = function(element) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.documentElement.style.filter = "invert(80%)";'}
-        )
+applyButton.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id, allFrames: true },
+            func: () => {
+                document.body.style.background = "#fff";
+                document.documentElement.style.filter = "invert(80%)";
+                var style = document.createElement("style"); 
+                style.innerHTML = "img {filter: invert(1);}"; 
+                document.body.append(style);
+            }
+        })
     })
 }
 
-removeButton.onclick = function(element) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.documentElement.style.filter = "initial";'}
-        )
+removeButton.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id, allFrames: true },
+            func: () => {
+                document.documentElement.style.filter = "initial";
+            }
+        })
     })
 }
