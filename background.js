ff940148a871d68a050e2f5e77526cc93542db06
hoang <hoangcoltech@gmail.com>
2021-12-17 05:13:42 +0700

    Update background.js

--- background.js
+++ background.js
@@ -4,10 +4,12 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         chrome.scripting.executeScript({
             target: { tabId, allFrames: true },
             func: () => {
+                let orgBg = document.body.style.background || "empty";
                 document.body.style.background = "#fff";
                 document.documentElement.style.filter = "invert(80%)";
                 var style = document.createElement("style"); 
                 style.setAttribute("id", "hoangtv-darkmode-style");
+                style.setAttribute("hoangtv-orgBg", orgBg);
                 style.innerHTML = "img {filter: invert(1);}"; 
                 document.body.append(style);
             }
