182cb30b8ee2adff5392e4d614a36097c5acdc3b
hoang <hoangcoltech@gmail.com>
2021-12-17 04:14:36 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+
+        chrome.scripting.executeScript({
+            target: { tabId, allFrames: true },
+            func: () => {
+                document.body.style.background = "#fff";
+                document.documentElement.style.filter = "invert(80%)";
+                var style = document.createElement("style"); 
+                style.innerHTML = "img {filter: invert(1);}"; 
+                document.body.append(style);
+            }
+        })
+
+    }
+})
