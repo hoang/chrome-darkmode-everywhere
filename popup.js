33fa9f576e694d5476b4f5003bf55940e7fc220e
hoang <hoangcoltech@gmail.com>
2021-12-17 05:13:20 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,10 +6,12 @@ applyButton.onclick = function (element) {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id, allFrames: true },
             func: () => {
+                let orgBg = document.body.style.background || "empty";
                 document.body.style.background = "#fff";
                 document.documentElement.style.filter = "invert(80%)";
-                var style = document.createElement("style");
+                var style = document.createElement("style"); 
                 style.setAttribute("id", "hoangtv-darkmode-style");
+                style.setAttribute("hoangtv-orgBg", orgBg);
                 style.innerHTML = "img {filter: invert(1);}"; 
                 document.body.append(style);
             }
@@ -23,7 +25,13 @@ removeButton.onclick = function (element) {
             target: { tabId: tabs[0].id, allFrames: true },
             func: () => {
                 document.documentElement.style.filter = "initial";
-                document.getElementById("hoangtv-darkmode-style").remove();
+                let styleElm = document.getElementById("hoangtv-darkmode-style");
+                let orgBg = styleElm.getAttribute("hoangtv-orgBg");
+                if (orgBg != "empty")
+                    document.body.style.background = orgBg;
+                else
+                    document.body.style.background = null;
+                styleElm.remove();
             }
         })
     })
